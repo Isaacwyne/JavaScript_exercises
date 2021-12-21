c5004969982961f95db2a7251352d3c419738e4c
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2021-12-21 15:41:53 +0300

    a simple program to keep count of a certain character

---
+++ countChars.js
@@ -0,0 +1,14 @@
+function countChars(str, character) {
+  // set a counter variable to keep track of the given charater
+  let counter = 0;
+
+  // iterate over the "str" variable for its elements
+  for (let element of str) {
+    if (element == character) {
+      counter++;
+    }
+  }
+  console.log(counter);
+}
+
+countChars("banana", "a");
