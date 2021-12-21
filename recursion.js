82e963e0768b06a0776929dd6877917800fee29a
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2021-12-21 13:42:40 +0300

    adding comments to new file

--- recursion.js
+++ recursion.js
@@ -1,10 +1,11 @@
 function isEven(num) {
+  // to avoid all negative numbers
   if (num < 0) {
     return;
   } else if (num === 0) {
-    return true;
+    return true; // base case for when the remainder of num is = 0 (num is even)
   } else if (num === 1) {
-    return false;
+    return false; // base case for when the remainder of num is = 1 (num is odd)
   } else {
     return isEven(num % 2);
   }
