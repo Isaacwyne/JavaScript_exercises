9df52458efe07a7bf6f5c9648b9c761fb664e726
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2022-01-06 21:19:03 +0300

    created the sum function & added support for negative nums

--- range.js
+++ range.js
@@ -1,23 +1,36 @@
-function range(a, b) {
-    // declaring an empty array
-    let numbers = [];
+// declare the range function
+function range(a, b, step = 1) {
+  // declare an empty array
+  let numbers = [];
 
-    //loop through the numbers given to determine; the numbers in between
-    for (let i = a; i <= b; i++){
-        // appending each number found to the empty array created earlier
-        numbers.push(i);
+  // check if the "step" is less than 0
+  if (step < 0) {
+    for (let negative = a; negative >= b; negative += step) {
+      numbers.push(negative);
     }
     return numbers;
+  } else {
+    for (let positive = a; positive <= b; positive += step) {
+      numbers.push(positive);
+    }
+    return numbers;
+  }
 }
 
-let array = range(1, 10);
-
 function sum(array) {
+  // declare a variable to keep track of the sum of the numbers
+  let result = 0;
+  // check if the arrayl is empty and if so => just return
   if (array == []) {
     return;
   } else {
-    for (let i = 0; i <= array.length; i++) {
-      console.log(i);
+    // loop through the array and on each iteration increment the "result" variable
+    for (let item of array) {
+      result += item;
     }
+    return result;
   }
 }
+
+console.log(sum(range(1, 10, 2)));
+console.log(sum(range(5, 2, -1)));
