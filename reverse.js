9f4755afb69df48757de15f214b564737a05815c
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2022-01-09 21:44:53 +0300

    reversed the array in place

--- reverse.js
+++ reverse.js
@@ -6,4 +6,19 @@ function reverseArray(array) {
   return reversed;
 }
 
+function reverseArrayInPlace(array) {
+  const n = array.length - 1;
+  const midPoint = Math.floor(n / 2);
+
+  for (let i = 0; i < midPoint; i++) {
+    // flipping elements here
+    // creating a variable to temporarily store the characters
+    let temp = array[i];
+    array[i] = array[n - i];
+    array[n - i] = temp;
+  }
+  return array;
+}
+
 console.log(reverseArray([1, 2, 3, 4]));
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
